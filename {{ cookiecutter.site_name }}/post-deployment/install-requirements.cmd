4494f768b857b81d78a0ee50af6839609ac5a36d
Brett Cannon <brett@python.org>
2016-08-11 17:27:25 -0700

    And yet even more iterative tests

--- {{ cookiecutter.site_name }}/post-deployment/install-requirements.cmd
+++ {{ cookiecutter.site_name }}/post-deployment/install-requirements.cmd
@@ -1,2 +1,2 @@
 :: cwd == D:\home\site\repository
-call D:\home\Python{{ cookiecutter.python_version|replace('.', '')|truncate(2, end='') }}\python.exe -m pip --disable-pip-version-check install -r {{ cookiecutter.requirements_filename }}
+call D:\home\Python{{ cookiecutter.python_version|replace('.', '')|truncate(2, end='') }}\python.exe -m pip --disable-pip-version-check install -r D:\home\site\wwwroot\{{ cookiecutter.requirements_filename }}
