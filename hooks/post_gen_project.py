cd7ab5960cbae89599c35c334cc5f91132ab2061
Brett Cannon <brett@python.org>
2016-08-16 11:05:49 -0700

    Rename deploy.json to azuredeploy.json to match what deploy.azure.com wants

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,4 @@ site_extensions_base_url = "{}"
 extension_name = "Python{}{}".format(python_version.replace(".", ""), cpu_arch)
 
 print()
-print("**NOTE**: Don't forget to deploy the deploy.json ARM template.")
+print("**NOTE**: Don't forget to deploy the azuredeploy.json ARM template.")
