898c71b134500a0f80ac8f3ba8313ee545a2fe9a
Brett Cannon <brett@python.org>
2016-08-25 09:43:17 -0700

    Remove dead code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,2 @@
-python_version = "{{ cookiecutter.python_version }}"
-cpu_arch = "{{ cookiecutter.cpu_arch }}"
-
-site_extensions_base_url = "{}"
-extension_name = "Python{}{}".format(python_version.replace(".", ""), cpu_arch)
-
 print()
 print("**NOTE**: Don't forget to deploy the azuredeploy.json ARM template.")
