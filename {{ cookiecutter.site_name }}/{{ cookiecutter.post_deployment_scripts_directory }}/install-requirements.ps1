b44d98ab1c270cd1cbbe493ac0ca302ac73ec9dd
Brett Cannon <brett@python.org>
2016-11-08 13:55:57 -0800

    Try to centralize template-spanning items

--- {{ cookiecutter.site_name }}/{{ cookiecutter.post_deployment_scripts_directory }}/install-requirements.ps1
+++ {{ cookiecutter.site_name }}/{{ cookiecutter.post_deployment_scripts_directory }}/install-requirements.ps1
@@ -1,5 +1,5 @@
 # $pwd == D:\home\site\repository
-$PYTHON = 'D:\home\Python{{ cookiecutter.python_version|replace('.', '')|truncate(2, end='') }}\python.exe'
+$PYTHON = '{{ cookiecutter._python_dir }}\python.exe'
 $AllArgs = @('-m', 'pip',
              '--disable-pip-version-check',
              'install', '--upgrade', '-r', 'D:\home\site\wwwroot\{{ cookiecutter.requirements_file_name }}')
